b240b0b25223b8fcee068d13e4b93d297c425677
obwan02 <obwan02@hotmail.co.nz>
2023-05-30 12:24:40 +1200

    update: Making ClothingItemActivity dynamic

--- app/src/main/java/com/example/clothingapp/activities/ClothingItemActivity.java
+++ app/src/main/java/com/example/clothingapp/activities/ClothingItemActivity.java
@@ -1,16 +1,64 @@
 package com.example.clothingapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.clothingapp.R;
+import com.example.clothingapp.data.ClothesSize;
+import com.example.clothingapp.data.ClothingItem;
+import com.example.clothingapp.data.IProvider;
+import com.example.clothingapp.data.NullProvider;
+
+import java.util.List;
 
 public class ClothingItemActivity extends AppCompatActivity {
+
+    public static final String INTENT_CLOTHING_ITEM_KEY = "clothingItem";
+
+    private static class ViewHolder {
+        private final ImageView image;
+        private final TextView price;
+        private final TextView title;
+
+        public ViewHolder(Activity activity) {
+            this.image = activity.findViewById(R.id.clothing_item_image);
+            this.price = activity.findViewById(R.id.clothing_item_price);
+            this.title = activity.findViewById(R.id.clothing_item_title);
+        }
+    }
+
+    private ViewHolder vh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clothing_item);
+
+        vh = new ViewHolder(this);
+
+        var intent = getIntent();
+        var item = (ClothingItem) intent.getSerializableExtra(INTENT_CLOTHING_ITEM_KEY);
+
+        if(item == null) {
+            item = new ClothingItem("NOTHING",
+                                -1.0f,
+                                    ClothesSize.MEDIUM,
+                                    List.of(),
+                          "No clothing item passed to activity",
+                            "NULL");
+            // Warn if no provider is sent
+            Log.w(getPackageName(), "No ClothingItem passed to ClothingItemActivity");
+        }
+
+
+
+
     }
 }
